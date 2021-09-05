3e46d2439e70738eb1a02d0e567732c5acd31229
bnestes18 <bnestes2014@gmail.com>
2021-09-04 21:38:12 -0400

    Converted old fashioned function into an IIFE

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-/**
- * Get a date in the future (or past) from a timestamp
- * @return {String} A formatted date string
- */
-function getFutureDate (options = {}) {
-
-    let { num, units, timestamp, format } = Object.assign({
-        num: 1,
-        units: 'days',
-        timestamp: new Date().getTime(),
-        format: {
-            dateStyle: 'long', 
-            timeStyle: 'short', 
-            hour12: true
-        }
-    }, options);
+let stamp = (function() {
 
     let times = {
         hours: 1000 * 60 * 60,
@@ -22,14 +7,74 @@ function getFutureDate (options = {}) {
         months: 1000 * 60 * 60 * 24 * 30,
         years: 1000 * 60 * 60 * 24 * 365
     }
-    
 
-    if (!times[units]) { throw new Error(`${times[units]} is not a valid unit of time.`) };
+    let format = {
+        dateStyle: 'long', 
+        timeStyle: 'short', 
+        hour12: true
+    }
 
-    let newTimestamp = timestamp + (times[units] * num);
-    let newDate = new Date(newTimestamp);
+    /**
+    * Add n number of hours to the given timestamp
+    * @return {Number} A unix timestamp for hours
+    */
+    function addHours(timestamp, num) {
+        let hours = times['hours'];
+        return timestamp + (hours * num);
+    }
 
-    return newDate.toLocaleString(navigator.language, format);
-}
+    /**
+    * Add n number of days to the given timestamp
+    * @return {Number} A unix timestamp for days
+    */
+    function addDays(timestamp, num) {
+        let days = times['days'];
+        return timestamp + (days * num);
+    }
 
-getFutureDate({num: 2, units: 'years'});
+    /**
+    * Add n number of weeks to the given timestamp
+    * @return {Number} A unix timestamp for weeks
+    */
+    function addWeeks(timestamp, num) {
+        let weeks = times['weeks'];
+        return timestamp + (weeks * num);
+    }
+
+    /**
+    * Add n number of months to the given timestamp
+    * @return {Number} A unix timestamp for months
+    */
+    function addMonths(timestamp, num) {
+        let months = times['months'];
+        return timestamp + (months * num);
+    }
+
+    /**
+    * Add n number of years to the given timestamp
+    * @return {Number} A unix timestamp for years
+    */
+    function addYears(timestamp, num) {
+        let years = times['years'];
+        return timestamp + (years * num);
+    }
+
+    /**
+    * Get a date in the future (or past) from a timestamp
+    * @return {String} A formatted date string
+    */
+    function getDate(timestamp) {
+        let date = new Date(timestamp);
+        return date.toLocaleString(navigator.language, format);
+    }
+
+    // Export functions
+    return {addHours, addDays, addWeeks, addMonths, addYears, getDate}
+
+})();
+
+let now = new Date().getTime();
+console.log('now', now);
+let addTwoYears = stamp.addYears(now, 2);
+console.log(addTwoYears);
+console.log(stamp.getDate(addTwoYears));
